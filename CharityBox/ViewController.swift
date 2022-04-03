a3c2a97e678a5524bbf585ddaff5b9b715fd70d4
mert-celik-1 <mertcelik2399@gmail.com>
2022-04-03 13:53:12 +0300

    Initial Commit

---
+++ CharityBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharityBox
+//
+//  Created by Mert Çelik on 3.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
